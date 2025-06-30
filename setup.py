e08a0b3c4f14c953ab0ce1ae39287631cc29b834
DheerajVarma31 <dheerajiitian31@gmail.com>
2025-06-30 09:28:44 +0530

    updated

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages,setup
+
+setup(
+    name="Medical chatbot",
+    version="0.0.0",
+    author="Dheeraj Varma",
+    author_email="dheerajiitian31@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
