a784cdafce255549490b3dc3db578faf203f49b9
DheerajVarma31 <dheerajiitian31@gmail.com>
2025-06-30 11:33:47 +0530

    updated

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask,render_template,jsonify,request
 from src.helper import download_hugging_face_embeddings
 from langchain.vectorstores import Pinecone
 import pinecone
-from pinecone.grpc import PineconeGRPC as Pinecone
+from langchain_pinecone import Pinecone as LangchainPinecone
 from pinecone import ServerlessSpec
 from langchain.chains import RetrievalQA
 from langchain.llms import CTransformers
